fc036376f77d5683888553c004a30d42b351ac35
iammouaz <88675267+iammouaz@users.noreply.github.com>
2024-04-21 20:37:15 +0300

    add business  logic for todo function

---
+++ blocs/todosBloc.ts
@@ -0,0 +1,27 @@
+import { Dispatch } from "redux";
+import { RootState } from "../store/configureStore";
+import { addTodo, toggleTodo } from "../store/todosSlice";
+
+class TodosBloc {
+  private dispatch: Dispatch;
+  private getState: () => RootState;
+
+  constructor(dispatch: Dispatch, getState: () => RootState) {
+    this.dispatch = dispatch;
+    this.getState = getState;
+  }
+
+  addTodo = (text: string) => {
+    this.dispatch(addTodo(text));
+  };
+
+  toggleTodo = (id: number) => {
+    this.dispatch(toggleTodo(id));
+  };
+
+  getTodos = () => {
+    return this.getState().todos.todos;
+  };
+}
+
+export default TodosBloc;
